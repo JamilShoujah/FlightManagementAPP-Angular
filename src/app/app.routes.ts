43a59930d5db237745aab1e62de4bdebc1d5dbba
JamilShoujah <jws09@mail.aub.edu>
2026-01-29 13:55:49 +0200

    landing page complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ViewFlights } from './pages/view-flights/view-flights';
+import { ViewOrders } from './pages/view-orders/view-orders';
+import { LandingPage } from './pages/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPage },
+  { path: 'flights', component: ViewFlights },
+  { path: 'orders', component: ViewOrders },
+];
---
+++ src/app/pages/view-flights/view-flights.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-flights',
+  imports: [],
+  templateUrl: './view-flights.html',
+  styleUrl: './view-flights.scss',
+})
+export class ViewFlights {
+
+}
